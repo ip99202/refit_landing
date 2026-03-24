84de1ab50ad5f5fe61b0f08b85e26028059e00b8
Janghee Han <ip99202@naver.com>
2026-03-24 13:32:17 +0900

    feat: add Google Tag Manager (GTM) script and noscript iframe

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,15 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WS4HT7WN');`}
+        </Script>
+        {/* End Google Tag Manager */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-ZHWWD70WSG"
           strategy="afterInteractive"
@@ -42,6 +51,16 @@ export default function RootLayout({
         </Script>
       </head>
       <body className={`${notoSansKR.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WS4HT7WN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
